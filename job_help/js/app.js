eafce79eebca3aa15c1ed3eb695bddc1032757a8
yangyu <jxndyy2014@163.com>
2016-08-12 15:23:06 +0800

    与服务器功能同步
    与服务器功能同步

--- job_help/js/app.js
+++ job_help/js/app.js
@@ -1,27 +1,237 @@
-function selectStu(database,username) {
-	database.transaction(function(tx) {
-		tx.executeSql(
-			"select * from student where director = ?", [username],
-			function(tx, result) {
-				for(var i = 0; i < result.rows.length; i++) {
-					getStu(result.rows.item(i));
-				}
-			}, null
-		);
-	});
+function geturl(){
+	var url = "http://172.16.120.36:8080/Guidance/";
+	return url;
 }
 
+function selectStu(username) {
+	
+	var url = geturl()+"student/getstudentBydrt";
+	var directornanme = username;
+	var stu = {
+		director: directornanme
+	};
+	mui.ajax(url, {
+		data: stu,
+		dataType: 'json', //服务器返回json格式数据
+		type: 'post', //HTTP请求类型
+		success: function(data) {
+			//console.info(JSON.stringify(data));
+			for(var i = 0; i < data.studentList.length; i++) {
+				//console.info(data.studentList[i].username); 
+				getStu(data.studentList[i]);
+			}
+
+		},
+		error: function(xhr, type, errorThrown) {
+			//异常处理；
+			console.log(type);
+		}
+	});
+
+}
+
+function selectStuByid(id) {
+
+	var url = geturl()+"student/getstudentByid";
+	var stu = {
+		id: id
+	};
+	mui.ajax(url, {
+		data: stu,
+		dataType: 'json', //服务器返回json格式数据
+		type: 'post', //HTTP请求类型
+		success: function(data) {
+			setStuByid(data.student);
+		},
+		error: function(xhr, type, errorThrown) {
+			//异常处理；
+			console.log(type);
+		}
+	});
+
+}
+
+function setStuByid(stuInfo) {
+	var username = stuInfo.username;
+	var name = stuInfo.name;
+	var department = stuInfo.department;
+	var id = stuInfo.id;
+    var picpath = stuInfo.picpath;
+	var modifyWebview = mui.openWindow({
+		url: 'modifyStudent.html',
+		id: 'modifyStudent',
+		extras: {
+			username: username,
+			name1: name,
+			department: department,
+			userID: id,
+			picpath:picpath
+		}
+	});
+}function deleteStu(){
+//var a=	this.getAttribute('class')
+	alert("aaaaa");
+}
 function getStu(stuInfo) {
 	var name = stuInfo.name;
 	var username = stuInfo.username;
 	var department = stuInfo.department;
 	var director = stuInfo.director;
+	var id = stuInfo.id;
+	var picpath = stuInfo.picpath;
 	var innerHtml = '<li class="mui-table-view-cell mui-media">';
-	innerHtml += '<a href="javascript:;">';
-	innerHtml += '<img class="mui-media-object mui-pull-left" src="images/qq.png">';
+	innerHtml += '<div class="mui-slider-right mui-disabled deletestu">'
+	innerHtml += '<a class="mui-btn mui-btn-red" href="' + id + '">删除</a></div>'
+	innerHtml += '<div class="mui-slider-handle">'
+	innerHtml += '<a href="' + id + '" class="getid">';
+	innerHtml += '<img class="mui-media-object mui-pull-left" src="'+picpath+'">';
 	innerHtml += '<div class="mui-media-body">' + name;
 	innerHtml += '<p class="mui-ellipsis">' + '科室：' + department + '</p>';
 	innerHtml += '<p class="mui-ellipsis">' + '指导老师：' + director + '</p>';
-	innerHtml += '</div></a></li>';
+	innerHtml += '</div></a></div></li>';
 	$('#StuList').append(innerHtml);
+}
+
+function removeAllItem() {
+	$('.mui-table-view-cell').remove();
+}
+
+function seletTrain(account){
+	
+	var url = geturl()+"train/getTrainInfo";
+	var stu = {
+		director: account 
+	};
+	mui.ajax(url, {
+		data: stu,
+		dataType: 'json', //服务器返回json格式数据
+		type: 'post', //HTTP请求类型
+		success: function(data) {
+			for (var i = 0 ; i<data.length;i++) {
+				  addTrainItem(data[i]);
+			}
+         
+		},
+		error: function(xhr, type, errorThrown) {
+			//异常处理；
+			console.log(type);
+		}
+	});
+
+}
+function addTrainItem(data){
+	var inner = '';
+	inner += '<li class="mui-table-view-cell"><lable style="display:none;" class="idVal">'+data.id+'</lable><div class="mui-slider-right mui-disabled deletestu">';
+	inner += '<a class="mui-btn mui-btn-red" href="' + data.id + '">删除</a></div>'
+	inner += '<div class="mui-slider-handle">'
+	inner += '<p id="title">'+data.title+'</p><span id="data">'+data.startDate+'至'+data.endDate+'</span>';
+	inner += '<p id="content">'+data.content+'</p>';
+	inner += '<span>学生：'+data.studentVal+'</span></div></li>';
+	
+	$('#TrainList').append(inner);
+
+}
+function addTrain(trainInfo,callback){
+	if(trainInfo.title.length == 0 ){
+		callback('标题不能为空');
+		return callback;
+	}
+		if(trainInfo.content.length == 0 ){
+			callback('内容不能为空');
+		return callback;
+		}if(trainInfo.startDate.length == 0 ){
+			callback('起始日期不能为空');
+		return callback;
+		}if(trainInfo.endDate.length == 0){
+			callback('结束日期不能为空');
+		return callback;
+		}if(trainInfo.student.length == 0){
+			callback('培训学生不能为空');
+		return callback;
+		}
+		var url = geturl()+"train/insertTrain";
+			mui.ajax(url, {
+				data: trainInfo,
+				dataType: 'json', //服务器返回json格式数据
+				type: 'post', //HTTP请求类型
+				success: function(data) {
+					if(data == "1"){
+						plus.nativeUI.toast('添加成功');
+						mui.back();
+					}else{
+					plus.nativeUI.toast('添加失败');
+
+					}
+
+				},
+				error: function(xhr, type, errorThrown) {
+					//异常处理；
+					console.log(type);	
+					return callback;
+				}
+			});
+
+}
+
+function getstudentData(userPicker){
+	
+			var user = getstate();
+			var director = {
+				director: user.account
+			}
+			var url = geturl()+"student/selectStuforTrain";
+			mui.ajax(url, {
+				data: director,
+				dataType: 'json', //服务器返回json格式数据
+				type: 'post', //HTTP请求类型
+				success: function(data) {
+					userPicker.setData(data);
+
+				},
+				error: function(xhr, type, errorThrown) {
+					//异常处理；
+					console.log(type);
+				}
+			});
+}
+function modifyTrain(trainInfo,callback){
+	if(trainInfo.title.length == 0 ){
+		callback('标题不能为空');
+		return callback;
+	}
+		if(trainInfo.content.length == 0 ){
+			callback('内容不能为空');
+		return callback;
+		}if(trainInfo.startDate.length == 0 ){
+			callback('起始日期不能为空');
+		return callback;
+		}if(trainInfo.endDate.length == 0){
+			callback('结束日期不能为空');
+		return callback;
+		}if(trainInfo.student.length == 0){
+			callback('培训学生不能为空');
+		return callback;
+		}
+		var url = geturl()+"train/updateTrain";
+			mui.ajax(url, {
+				data: trainInfo,
+				dataType: 'json', //服务器返回json格式数据
+				type: 'post', //HTTP请求类型
+				success: function(data) {
+					if(data == "1"){
+						plus.nativeUI.toast('修改成功');
+						mui.back();
+					}else{
+					plus.nativeUI.toast('修改失败');
+
+					}
+
+				},
+				error: function(xhr, type, errorThrown) {
+					//异常处理；
+					console.log(type);	
+					return callback;
+				}
+			});
+
 }
--- job_help/js/login.js
+++ job_help/js/login.js
@@ -1,24 +1,23 @@
-
 /*
  新用户注册
  * */
 function reg(regInfo, callback) {
 	var checkEmail = function(email) {
 		email = email || '';
-		return (email.length > 3 && email.indexOf('@') > -1);
+		return(email.length > 3 && email.indexOf('@') > -1);
 	};
-	
+
 	regInfo.account = regInfo.account || '';
 	regInfo.password = regInfo.password || '';
-	if(regInfo.account.length<5){
+	if(regInfo.account.length < 5) {
 		callback('账号字符不能小于5');
 		return callback;
 	}
-	if(regInfo.password.length<5){
+	if(regInfo.password.length < 5) {
 		callback('密码不能小于5个字符');
 		return callback;
 	}
-	if(!checkEmail(regInfo.email)){
+	if(!checkEmail(regInfo.email)) {
 		callback('邮箱输入不合法');
 		return callback;
 	}
@@ -27,62 +26,92 @@ function reg(regInfo, callback) {
 	/*localStorage.setItem('account',account);
 	localStorage.setItem('password',password);*/
 	//获取users的信息，把json字符串格式转换成json对象
-	var users = JSON.parse(localStorage.getItem('users')||'[]');
-	if(users!==null){
-	var same = users.some(function(user){
-		return user.account == regInfo.account;
-	});
-	if(same){
-		callback('账户已被注册');
-		return callback;
+	var url = geturl()+"teacher/addTeacher";
+	var reg = {
+		teaName: regInfo.account,
+		teaPassword: regInfo.password,
+		email: regInfo.email
 	}
-	}
-	//把新的信息放入到json中
-	users.push(regInfo);
-	//把新的users信息放入到本地存储数据库中
-	localStorage.setItem('users',JSON.stringify(users));
+	mui.ajax(url, {
+		data: reg,
+		dataType: 'json', //服务器返回json格式数据
+		type: 'post', //HTTP请求类型
+		success: function(data) {
+			if(data == "1") {
+
+				return callback();
+			}else if(data == "2"){
+				callback('用户名已被注册');
+				return callback;
+			}else{
+				callback('添加失败');
+				return callback;
+			}
+		},
+		error: function(xhr, type, errorThrown) {
 	
-	return callback();
+			//异常处理；
+			console.log(type);
+			callback('添加失败');
+				return callback;
+		}
+	});
 }
 /*
  用户登录
  * */
-function login(loginInfo,callback){
+function login(loginInfo, callback) {
 	loginInfo = loginInfo || {};
-	 loginInfo.account = loginInfo.account || '';
-	 loginInfo.password = loginInfo.password || '';
-	
-	if(loginInfo.account.length<5){
+	loginInfo.account = loginInfo.account || '';
+	loginInfo.password = loginInfo.password || '';
+
+	if(loginInfo.account.length < 5) {
 		callback('用户名字段不能小于5');
 		return callback;
 	}
-	if(loginInfo.password.length<5){
+	if(loginInfo.password.length < 5) {
 		callback('密码字段不能小于5');
 		return callback;
 	}
-	   
-	var users = JSON.parse(localStorage.getItem('users')||'[]');
-	//主要是some这个函数，它能够遍历数组，若是找到函数中合适的条件，会返回true，若是没有找到则返回false
-	var authed = users.some(function(user){
-		return user.account == loginInfo.account && user.password == loginInfo.password;
-	})
- 
-  
-   /*var account = localStorage.getItem('account');
-   var password = localStorage.getItem('password');
-   var authed = false;
-    if(loginInfo.account == account && loginInfo.password == password){
-    	  authed = true;
-    }*/
-	
-	if(authed){
-		createState(loginInfo.account);
-		return callback();
-	}else{
-		return callback('用户密码错误');
+
+   var url = geturl()+"teacher/selectByname";
+	var log = {
+		teaName: loginInfo.account,
+		teaPassword: loginInfo.password,
 	}
+	
+   mui.ajax(url, {
+		data: log,
+		dataType: 'json', //服务器返回json格式数据
+		type: 'post', //HTTP请求类型
+		success: function(data) {
+			if(data == "1") {
+               createState(loginInfo.account);
+		
+				return callback();
+			}else if(data == "2"){
+				callback('密码错误');
+				return callback;
+			}else if(data =="0"){
+				callback('用户不存在');
+				return callback;
+			}else{
+				callback('登录失败11111');
+				return callback;
+			}
+		},
+		error: function(xhr, type, errorThrown) {
+	
+			//异常处理；
+			console.log(type);
+			callback('登录失败');
+				return callback;
+		}
+	});
+
 }
-function createState(name){
+
+function createState(name) {
 	var state = getstate();
 	state.account = name;
 	state.state = 'logining';
@@ -92,18 +121,14 @@ function createState(name){
 /*
  获取状态
  * */
-function getstate(){
-	return JSON.parse(localStorage.getItem('stateInfo')||'{}');
+function getstate() {
+	return JSON.parse(localStorage.getItem('stateInfo') || '{}');
 }
 /*
  设置状态
  * */
-function setstate(stateInfo){
+function setstate(stateInfo) {
 	var state = stateInfo || {};
 	localStorage.setItem('stateInfo', JSON.stringify(stateInfo));
-	
-	
+
 }
-
-
-
